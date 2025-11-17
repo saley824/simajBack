2799a40a45722f052f0f9afb3152524e9543291a
sake <obradovica@lanaco.com>
2025-11-18 00:04:41 +0100

    added brand in response

--- src/controllers/devices_controller.ts
+++ src/controllers/devices_controller.ts
@@ -4,12 +4,19 @@ import { prisma } from "../server";
 const getAllDevices = async (req: Request, res: Response) => {
     const lang = req.headers["accept-language"] || "en";
     try {
-        const devices = await prisma.compatibleDevice.findMany({});
+        const devices = await prisma.compatibleDevice.findMany(
+            {
+                include: {
+                    brand: true
+                }
+            }
+        );
 
         res.status(200).json({
             success: true,
             data: {
                 devices: devices,
+
 
             },
         });
