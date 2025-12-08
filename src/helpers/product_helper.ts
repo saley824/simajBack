8816c6efb97d30e3e11e12e681c41660b977a3f0
sake <obradovica@lanaco.com>
2025-12-09 00:52:30 +0100

    fixed discount bam logic

--- src/helpers/product_helper.ts
+++ src/helpers/product_helper.ts
@@ -2,39 +2,33 @@ import { Currency, Product } from "@prisma/client";
 import { prisma } from "../server";
 
 async function formatProduct(p: Product, currency: Currency) {
-
-
-    const exchangeRate = await prisma.exchangeRate.findFirst(
-        {
-            where: {
-                currency: currency
-            }
-        }
-    )
-
+    const exchangeRate = await prisma.exchangeRate.findFirst({
+        where: { currency }
+    });
 
     const hasDiscount = p.discountPercent !== null && p.discountPercent > 0;
-    var finalPrice = hasDiscount
+
+    const sellingPriceBAM = p.sellingPrice!;
+    const finalPriceBAM = hasDiscount
         ? Number((p.sellingPrice! - (p.sellingPrice! * p.discountPercent!) / 100).toFixed(2))
-        : p.sellingPrice;
-    const sellingPriceBAM = p.sellingPrice;
-    const finalPriceBAM = finalPrice;
-    if (exchangeRate && p.sellingPrice && finalPrice) {
-        p.sellingPrice = Number((exchangeRate.rateFromBAM.toNumber() * p.sellingPrice!).toFixed(2));
-        finalPrice = Number((exchangeRate.rateFromBAM.toNumber() * finalPrice).toFixed(2));
+        : p.sellingPrice!;
+
+    let sellingPrice = sellingPriceBAM;
+    let finalPrice = finalPriceBAM;
+
+    if (exchangeRate) {
+        const rate = exchangeRate.rateFromBAM.toNumber();
+        sellingPrice = Number((sellingPriceBAM * rate).toFixed(2));
+        finalPrice = Number((finalPriceBAM * rate).toFixed(2));
     }
-
-
-
-
 
     return {
         ...p,
-        hasDiscount,
+        sellingPrice,
         finalPrice,
         sellingPriceBAM,
-        finalPriceBAM
-
+        finalPriceBAM,
+        hasDiscount
     };
 }
 function getFinalPrice(sellingPrice: number, discountPercent: number): number {
